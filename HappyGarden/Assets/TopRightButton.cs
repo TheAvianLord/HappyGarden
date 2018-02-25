79f2f4dbe95e4cba5657877296f0460d5adfe191
TheAvianLord <angela-yixuan21@hotmail.com>
2018-02-25 07:03:44 -0800

    Menu Added

---
+++ HappyGarden/Assets/TopRightButton.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TopRightButton : MonoBehaviour
+{
+    public GameObject instructions;
+    public void OnTriggerEnter()
+    {
+        instructions.SetActive(true);
+    }
+
+    public void OnTriggerLeave()
+    {
+        instructions.SetActive(false);
+    }
+}
---
+++ HappyGarden/Assets/enabledisable.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class enabledisable : MonoBehaviour {
+
+	public GameObject gma;
+	public GameObject gpa;
+
+	bool trace = false;
+	bool tracker = false;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		
+	}
+
+	public void popUp()
+	{
+		if (tracker == false)
+		{
+			gma.SetActive (true);
+			tracker = true;
+		}
+		else if( tracker == true)
+		{
+			gma.SetActive (false);
+			tracker = false;
+		}
+	}
+
+	public void popUp2()
+	{
+		if (trace == false)
+		{
+			gpa.SetActive (true);
+			trace = true;
+		}
+		else if( trace == true)
+		{
+			gpa.SetActive (false);
+			trace = false;
+		}
+	}
+
+    public void playGame()
+    {
+        SceneManager.LoadScene("Leap Hands Demo (Desktop) 1");
+    }
+}
